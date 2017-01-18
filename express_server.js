33fc55beae07754b221a9b405f4e25f9cd341b4b
anotherwan <itsanotherwan@gmail.com>
2017-01-17 21:42:24 -0500

    form posts to db, added delete /urls/:id

--- express_server.js
+++ express_server.js
@@ -3,16 +3,25 @@
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
+const methodOverride = require('method-override')
 const PORT = process.env.PORT || 8080
 const CHARS = generateRandomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
 app.set("view engine", "ejs")
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
-  "mrrOOa": "hhtp://www.soundcloud.com"
+  "mrrOOa": "http://www.soundcloud.com"
 };
 
 function generateRandomString(length, CHARS) {
@@ -31,9 +40,11 @@ app.get("/urls", (req, res) => {
   res.render('urls_index', { urls: urlDatabase })
 });
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.delete("/urls/:id", (req, res) => {
+  console.log('Deleting')
+  delete urlDatabase[req.params.id]
+  res.redirect('/urls')
+})
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -45,15 +56,21 @@ app.get("/urls/:id", (req, res) => {
 
 
 app.post("/urls", (req, res) => {
+  // console.log(req.body)
   let shortURL = generateRandomString(6, CHARS)
-  res.redirect(`http://localhost:8080/urls/${shortURL}`)
+  let longUrl = req.body.longURL
+  urlDatabase[shortURL] = longUrl
+  res.redirect(`/urls/${shortURL}`)
 })
 
 app.get("/u/:shortURL", (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]
-  // console.log(longURL)
   res.redirect(longURL)
 })
+
+// app.get("/urls.json", (req, res) => {
+//   res.json(urlDatabase);
+// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
