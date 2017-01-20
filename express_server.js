0804651817878727c8d3489b0ce25442e324fc69
anotherwan <itsanotherwan@gmail.com>
2017-01-20 16:26:16 -0500

    added proper /register and /login post/put routes. only logged in users can create routes

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
 'use strict'
 
-const express = require("express")
+const express = require('express')
 const app = express()
-const bodyParser = require("body-parser")
-const cookieParser = require("cookie-parser")
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const methodOverride = require('method-override')
 const PORT = process.env.PORT || 8080
 const CHARS = generateRandomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+// const bcrypt = require('bcrypt')
+// const hashed_password = bcrypt.hashSync(password, 10)
 
-app.set("view engine", "ejs")
+
+app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(methodOverride(function (req, res) {
@@ -21,23 +24,29 @@ app.use(methodOverride(function (req, res) {
 }))
 
 
-const urlDatabase = {
-                    "b2xVn2": "http://www.lighthouselabs.ca",
-                    "9sm5xK": "http://www.google.com",
-                    "mrrOOa": "http://www.soundcloud.com"
-                  };
+global.urlDatabase = {
+  'b2xVn2': {
+    longURL: 'http://www.lighthouselabs.ca',
+    userID: 'userRandomID'
+  },
+  '9sm5xK': {
+    longURL: 'http://www.google.com',
+    userID: 'user2RandomID'
+  }
+
+}
 
 global.usersDatabase = {
-  "user_random_id_1": {
-                    id: "userRandomID",
-                    email: "user@example.com",
-                    password: "1234"
-                  },
-  "user_random_id_2": {
-                    id: "user2RandomID",
-                    email: "user2@example.com",
-                    password: "4321"
-                  }
+  'userRandomID': {
+    id: 'userRandomID',
+    email: 'user@example.com',
+    password: '1234'
+  },
+  'user2RandomID': {
+    id: 'user2RandomID',
+    email: 'user2@example.com',
+    password: '4321'
+  }
 }
 
 function generateRandomString(length, CHARS) {
@@ -48,46 +57,68 @@ function generateRandomString(length, CHARS) {
   return result
 }
 
+function findUserEmail(req) {
+  let foundUser = null
+  for (let userId in usersDatabase) {
+    if (usersDatabase[userId].email === req.body.email) {
+      foundUser = usersDatabase[userId]
+      break
+    }
+  }
+  return foundUser
+}
 
-app.get("/urls", (req, res) => {
-  // console.log(req.cookies.user_email)
+function checkBlankParams(req) {
+  if (req.body.email === '' || req.body.password === '') {
+    res.status(403).send('Email or password must be filled in')
+  }
+}
+
+app.get('/urls', (req, res) => {
   res.render('urls_index', {
     urls: urlDatabase,
-    email: req.cookies.user_email
+    email: req.cookies.userEmail
    })
-});
+})
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
+app.get('/urls/new', (req, res) => {
+  let foundUser = req.cookies.userEmail
+  console.log(foundUser)
+  if (foundUser) {
+    res.render('urls_new', { userUrls: urlDatabase, email: foundUser })
+  } else {
+    res.redirect('/login')
+  }
 
-app.post("/urls", (req, res) => {
+})
+
+app.post('/urls', (req, res) => {
   let shortURL = generateRandomString(6, CHARS)
   let longUrl = req.body.longURL
   urlDatabase[shortURL] = longUrl
   res.redirect(`/urls/${shortURL}`)
 })
 
-app.get("/urls/:id", (req, res) => {
-  res.render("url_show", { shortURL: req.params.id, longURL: urlDatabase[req.params.id] });
-});
+app.get('/urls/:id', (req, res) => {
+  res.render('url_show', { shortURL: req.params.id, longURL: urlDatabase[req.params.id] })
+})
 
-app.get("/urls/:id/edit", (req, res) => {
-  res.render("url_show_edit", { shortURL: req.params.id, longURL: urlDatabase[req.params.id] })
-});
+app.get('/urls/:id/edit', (req, res) => {
+  res.render('url_show_edit', { shortURL: req.params.id, longURL: urlDatabase[req.params.id] })
+})
 
-app.put("/urls/:id", (req, res) => {
+app.put('/urls/:id', (req, res) => {
   let newLongURL = req.body.updatedLongURL
   urlDatabase[req.params.id] = newLongURL
   res.redirect('/urls')
 })
 
-app.delete("/urls/:id", (req, res) => {
+app.delete('/urls/:id', (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect('/urls')
 })
 
-app.get("/u/:shortURL", (req, res) => {
+app.get('/u/:shortURL', (req, res) => {
   let longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL)
 })
@@ -97,55 +128,20 @@ app.get('/login', (req, res) => {
   res.render('login')
 })
 
-app.put("/login", (req, res) => {
-  for (let user_id in usersDatabase) {
-    if (usersDatabase[user_id].email === req.body.email && usersDatabase[user_id].password === req.body.password) {
-      res.cookie('user_email', usersDatabase[user_id].email)
-      res.redirect('/urls')
-    } else {
-      res.status(403).send('Email and/or password do not match')
-    }
+app.put('/login', (req, res) => {
+  checkBlankParams(req)
+  let foundUser = findUserEmail(req)
+  if (!foundUser || foundUser.password != req.body.password) {
+    res.status(403).send('Incorrect email or password')
+  } else {
+    res.cookie('userEmail', foundUser.email)
+    res.redirect('/urls')
   }
-  // let emailMatch = false
-  // let passwordMatch = false
-  // for (let user_id in usersDatabase) {
-  //   if (usersDatabase[user_id].email === req.body.email) {
-  //     if (usersDatabase[user_id].password === req.body.password) {
-  //       emailMatch = true
-  //       passwordMatch = true
-  //     }
-  //   }
-  // }
-  // if (emailMatch === true && passwordMatch === true) {
-  //   res.cookie('user_email', req.body.email)
-  //   res.redirect('/urls')
-  // } else {
-  //   res.status(403).send('Email and/or password do not match!')
-  // }
 })
 
-// app.put("/login", (req, res) => {
-//   let emailMatch = false
-//   let passwordMatch = false
-//   for (let user_id in usersDatabase) {
-//     if (usersDatabase[user_id].email === req.body.email) {
-//       if (usersDatabase[user_id].password === req.body.password) {
-//         emailMatch = true
-//         passwordMatch = true
-//       }
-//     }
-//   }
-//   if (emailMatch === true && passwordMatch === true) {
-//     res.cookie('user_email', req.body.email)
-//     res.redirect('/urls')
-//   } else {
-//     res.status(400).send('Email and/or password do not match!')
-//   }
-// })
-
-app.post("/logout", (req, res) => {
-  res.clearCookie('user_email')
-  res.redirect('/urls')
+app.post('/logout', (req, res) => {
+  res.clearCookie('userEmail')
+  res.redirect('/login')
 })
 
 app.get('/register', (req, res) => {
@@ -153,39 +149,30 @@ app.get('/register', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  let randomUserId = generateRandomString(6, CHARS)
-  let emailExists = false;
-  for (var user_id in usersDatabase) {
-    if (usersDatabase[user_id].email === req.body.email) {
-      emailExists = true;
-    }
-  }
-  if (emailExists) {
-    res.status(400).send('Email already exists!')
-  } else if (req.body.email === "" || req.body.password === "") {
-    res.status(400).send('Please enter both an email and password!')
+  checkBlankParams(req)
+  let foundUser = findUserEmail(req)
+  if (foundUser) {
+    res.status(403).send('Email already exists!')
   } else {
-    res.cookie('user_email', req.body.email)
+    let randomUserId = generateRandomString(6, CHARS)
+    let foundUser = req.body.email
+    res.cookie('userEmail', foundUser)
     usersDatabase[randomUserId] = {
       id: randomUserId,
       email: req.body.email,
       password: req.body.password
     }
+    res.redirect('/urls')
   }
-  res.redirect('/urls')
 })
 
-
-
-
-
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+// app.get('/urls.json', (req, res) => {
+//   res.json(urlDatabase)
+// })
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
+  console.log(`Example app listening on port ${PORT}!`)
+})
 
 // get /
 //const current_user = req.signedCookies.current_user
